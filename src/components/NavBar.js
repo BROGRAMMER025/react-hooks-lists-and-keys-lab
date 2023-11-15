d955bdd3cecf843fef5dbefbd07ea2ec0f8a92d1
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-11-15 12:23:35 +0300

    Dne with assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return (
+    <div>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link.toLowerCase()}`}>
+          {link}
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default NavBar;
